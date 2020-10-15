d5d88b6faa967755211409ac819875f275954994
jd <duverseaujohnny21@gmail.com>
2020-10-15 19:14:19 -0400

    Fourth test pass

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -45,6 +45,20 @@ namespace VendorOrderTracker.Tests
       //Assert
       Assert.AreEqual(updatedPrice, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      List<Order> newList = new List<Order> { };
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
   
 
   }
