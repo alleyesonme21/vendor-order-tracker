b4227860d06b894f53a0825ae7a667c79461fb38
jd <duverseaujohnny21@gmail.com>
2020-10-15 20:48:31 -0400

    modified constructor, add 2 parameters, and constructor

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -6,12 +6,16 @@ namespace VendorOrderTracker.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public string Description {get; set;}
+    public int Phone {get; set;}
     public int Id { get; }
     public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDescription, int phoneNumber)
     {
       Name = vendorName;
+      Description = vendorDescription;
+      Phone = phoneNumber;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
