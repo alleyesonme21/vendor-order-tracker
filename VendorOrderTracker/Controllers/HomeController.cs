533bd7d429b859ca1fd014bb530f46520376a853
jd <duverseaujohnny21@gmail.com>
2020-10-16 23:43:40 -0400

    indented

--- VendorOrderTracker/Controllers/HomeController.cs
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -4,13 +4,10 @@ namespace VendorOrderTracker.Controllers
 {
   public class HomeController : Controller
   {
-
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
     }
-
-
   }
 }
