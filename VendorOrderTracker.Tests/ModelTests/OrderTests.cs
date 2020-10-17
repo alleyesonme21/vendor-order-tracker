4707b622823d6b93751b8649c9f4a39f36dd8611
jd <duverseaujohnny21@gmail.com>
2020-10-16 23:42:39 -0400

    indented

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -7,7 +7,6 @@ namespace VendorOrderTracker.Tests
   [TestClass]
   public class OrderTests : IDisposable
   {
-
     public void Dispose()
     {
       Order.ClearAll();
@@ -19,29 +18,27 @@ namespace VendorOrderTracker.Tests
       Order newOrder = new Order("test Title", "test description", 5, "test date");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
     [TestMethod]
     public void GetTitle_ReturnsTitle_String()
     {
       //Arrange
       Order newOrder = new Order("test Title", "test description", 5, "test date");
-
       //Act
       string result = newOrder.Title;
-
       //Assert
       Assert.AreEqual("test Title", result);
     }
+
     [TestMethod]
     public void SetPrice_SetPrice_String()
     {
       //Arrange
       Order newOrder = new Order("test Title", "test description", 5,  "test date");
-
       //Act
       int updatedPrice = 10;
       newOrder.Price = updatedPrice;
       int result = newOrder.Price;
-
       //Assert
       Assert.AreEqual(updatedPrice, result);
     }
@@ -51,10 +48,8 @@ namespace VendorOrderTracker.Tests
     {
       // Arrange
       List<Order> newList = new List<Order> { };
-
       // Act
       List<Order> result = Order.GetAll();
-
       // Assert
       CollectionAssert.AreEqual(newList, result);
     }
@@ -65,12 +60,9 @@ namespace VendorOrderTracker.Tests
       //Arrange
       Order newOrder1 = new Order("test Title", "test description", 5,  "test date");
       Order newOrder2 = new Order("test Title", "test description", 5,  "test date");
-    
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
-
       //Act
       List<Order> result = Order.GetAll();
-
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
@@ -82,13 +74,12 @@ namespace VendorOrderTracker.Tests
       Order newOrder1 = new Order("test Title", "test description", 5, "test date");
       Order newOrder2 = new Order("test address", "test guitar", 2, "test date");
       Order newOrder3 = new Order("test piano", "test bass", 4, "test date");
-
       //Act
       int result = newOrder3.Id;
-
       //Assert
       Assert.AreEqual(3, result);
     }
+
     [TestMethod]
     public void Find_ReturnsCorrectOrder_Order()
     {
@@ -101,8 +92,5 @@ namespace VendorOrderTracker.Tests
       //Assert
       Assert.AreEqual(newOrder1, result);
     }
-
-  
-
   }
 }
